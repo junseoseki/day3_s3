41638c967d6dcc752e0699fd3603234b77a50388
junseoseki <dhfkapap@naver.com>
2025-10-30 14:36:35 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,1 +1,9 @@
-
+for i in range(1,101):
+    if i % 2 == 0 and i % 5 == 0:
+        print("2,5의 배수입니다")
+    elif i % 2 == 0:
+        print("2의 배수입니다")
+    elif i % 5 == 0:
+        print("5의 배수입니다")
+    else:
+        print(i)
